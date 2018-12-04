7e3e82d90b9fb6b0a2f7a656e44ecf7af867a2d3
csr38 <cesar.tapia@alumnos.uv.cl>
2018-12-04 01:39:12 -0300

    2.0.6

--- ArchivoEntrada.java
+++ ArchivoEntrada.java
@@ -1,20 +1,45 @@
 import java.io.*;
 
 public class ArchivoEntrada extends Persona {
-    protected int ValorHotel;
+    protected String ValorHotel;
     protected String Fecha;
     protected String Habitaciones;
-    protected int Piso;
+    protected String Piso;
     protected String Servicio;
+    protected String cadena;
+    protected String n;
+    protected String n_1;
+    protected int numeroPiso;
+    protected int numeroHabitaciones;
     protected int Menu;
 
     public ArchivoEntrada() {
     }
 
     public void Inicializar_in() {
+        ValorHotel="100250.00";
+        Fecha = "22 10 2018";
+        Habitaciones="4";
+        Piso="2";
         try {
             FileWriter fichero = new FileWriter("inicializar.txt");
-            fichero.write("Holaaaaaaaaaaaaaa");
+            fichero.write(ValorHotel);
+            fichero.append("\r\n"); 
+            fichero.write(Fecha);
+            fichero.append("\r\n"); 
+            numeroHabitaciones = (int) (Math.random() * 10) + 1;
+            String n = Integer.toString(numeroHabitaciones);
+            fichero.write(n);
+            numeroPiso = (int) (Math.random() * 10) + 1;
+            String n_1 = Integer.toString(numeroPiso);
+            fichero.write(" "+n_1);
+            fichero.append("\r\n"); 
+            for (int i = 0; i < numeroPiso; i++) {
+                
+            }
+            fichero.write("4");
+            fichero.append("\r\n"); 
+            fichero.write("2");
             fichero.close();
 
         } catch (Exception ex) {
@@ -24,7 +49,13 @@ public class ArchivoEntrada extends Persona {
         try {
             FileReader lector = new FileReader("inicializar.txt");
             BufferedReader BR = new BufferedReader(lector);
-            System.out.println(BR.readLine());
+            
+            while((cadena=BR.readLine())!=null){
+                System.out.println(cadena);
+            }
+
+    
+            BR.close();
         } catch (Exception ex) {
             ex.printStackTrace();
             // TODO: handle exception
--- Hotel.java
+++ Hotel.java
@@ -4,7 +4,6 @@ public class Hotel {
     public static void main(String[] args) {
         ArchivoEntrada a = new ArchivoEntrada();
         a.Inicializar_in();
-
     }
 
 }
