a7afda7f866915711ff323a91a4464584f5de7b2
csr38 <cesar.tapia@alumnos.uv.cl>
2018-12-04 12:23:12 -0300

    2.0.6

--- ArchivoEntrada.java
+++ ArchivoEntrada.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArchivoEntrada extends Persona {
     protected String ValorHotel;
@@ -7,11 +9,16 @@ public class ArchivoEntrada extends Persona {
     protected String Piso;
     protected String Servicio;
     protected String cadena;
-    protected String n;
-    protected String n_1;
-    protected int numeroPiso;
-    protected int numeroHabitaciones;
+    protected String n;//Guarda el numero de abitaciones como string para luego guardarlo en fichero(Habitaciones)
+    protected String n_1;//Guarda el numero de pisos como string para luego guardarlo en fichero(Piso)
+    protected int NumeroPiso;
+    protected int NumeroHabitaciones;
+    protected int TipoDeHabitacion;
     protected int Menu;
+
+    
+    List<String> ListaTipoHabitacion = new ArrayList<>();
+
 
     public ArchivoEntrada() {
     }
@@ -19,27 +26,48 @@ public class ArchivoEntrada extends Persona {
     public void Inicializar_in() {
         ValorHotel="100250.00";
         Fecha = "22 10 2018";
-        Habitaciones="4";
-        Piso="2";
         try {
             FileWriter fichero = new FileWriter("inicializar.txt");
             fichero.write(ValorHotel);
             fichero.append("\r\n"); 
+
             fichero.write(Fecha);
-            fichero.append("\r\n"); 
-            numeroHabitaciones = (int) (Math.random() * 10) + 1;
-            String n = Integer.toString(numeroHabitaciones);
-            fichero.write(n);
-            numeroPiso = (int) (Math.random() * 10) + 1;
-            String n_1 = Integer.toString(numeroPiso);
+            fichero.append("\r\n");
+
+            NumeroPiso = (int) (Math.random() * 10) + 1;
+
+            String n_1 = Integer.toString(NumeroPiso);
             fichero.write(" "+n_1);
             fichero.append("\r\n"); 
-            for (int i = 0; i < numeroPiso; i++) {
-                
+
+
+            for (int x=0; x < NumeroPiso;x++){
+                NumeroHabitaciones = (int) (Math.random() * 10) + 1;
+                ListaTipoHabitacion.get(x).add(new ArrayList<String>());
+
+                for(int i=0; i < NumeroHabitaciones; i++){
+                    TipoDeHabitacion =(int) (Math.random() * 5) + 1;
+                    String Habitacion = Integer.toString(TipoDeHabitacion);
+                    
+                    ListaTipoHabitacion.get(i).add(Habitacion);
+                    
+                }
+
+                /*
+                String n = Integer.toString(NumeroHabitaciones);
+                fichero.write(n);
+                */
+                fichero.append("\r\n");
+
             }
-            fichero.write("4");
-            fichero.append("\r\n"); 
-            fichero.write("2");
+
+            
+            
+
+            
+
+            
+            
             fichero.close();
 
         } catch (Exception ex) {
